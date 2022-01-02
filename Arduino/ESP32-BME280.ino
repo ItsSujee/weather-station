9db16eee4d24561351d30cd7e07061c83eb10efd
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2022-01-02 01:40:31 -0500

    Upload Arduino Code

--- Arduino/ESP32-BME280.ino
+++ Arduino/ESP32-BME280.ino
@@ -7,6 +7,10 @@
 #include <PubSubClient.h>
 #include "SparkFunBME280.h"
 
+// Deep Sleep Details
+#define uS_TO_S_FACTOR 1000000ULL
+#define TIME_TO_SLEEP  1500
+
 // WiFi Details
 const char* ssid = "";
 const char* password = "";
@@ -27,38 +31,30 @@ PubSubClient client(espClient);
 void setup()
 {
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("ESP32 Ready...");
+  InitI2C();
   InitWifi();
   InitMQTT();
-  InitI2C();
   configTime(0, 0, ntpServer);
+  measureWeather();
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Going to sleep now");
+  Serial.flush(); 
+  esp_deep_sleep_start();
 }
 
 void loop() 
-{
-  while(WiFi.status() != WL_CONNECTED) {
-      InitWifi();
-    }
-  while (mySensor.beginI2C() != true) {
-      InitI2C();
-    }  
-  String m1 = String(getTime());
-  String m2 = String(mySensor.readTempC(), 3);
-  String m3 = String(mySensor.readFloatPressure(), 3);
-  String m4 = String(mySensor.readFloatAltitudeMeters(), 3);
-  String m5 = String(mySensor.readFloatHumidity(), 3);
-  String message = "{Time:"+ m1 +",Temperature(deg C):"+ m2 +",Pressure(Pa):"+ m3 +",Altitude(m):"+ m4 +",RelativeHumidity(%):"+ m5 +"}";
-  Serial.println(message);
-  client.publish(topic, message.c_str());
-  Serial.println("Message Published");
-  delay(25000);
+{ 
+  //  This is not going to be used 
 }
 
 void InitWifi()
 {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
     Serial.println("Connecting to WiFi...");
+    delay(2000);
   } 
 }
 
@@ -66,9 +62,8 @@ void InitMQTT()
 {
   client.setServer(mqtt_broker, mqtt_port);
   while (!client.connected()) {
-     String client_id = "esp32-client-";
-     client_id += String(WiFi.macAddress());
      Serial.println("Connecting to MQTT...");
+     String client_id = "esp32-client";
      if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
          Serial.println("MQTT connected");
      } else {
@@ -98,4 +93,17 @@ unsigned long getTime() {
   }
   time(&now);
   return now;
+}
+
+void measureWeather() 
+{
+  String m1 = String(getTime());
+  String m2 = String(mySensor.readTempC(), 2);
+  String m3 = String(mySensor.readFloatPressure(), 3);
+  String m4 = String(mySensor.readFloatAltitudeMeters(), 3);
+  String m5 = String(mySensor.readFloatHumidity(), 3);
+  String message = "{Time:"+ m1 +",Temperature(deg C):"+ m2 +",Pressure(Pa):"+ m3 +",Altitude(m):"+ m4 +",RelativeHumidity(%):"+ m5 +"}";
+  Serial.println(message);
+  client.publish(topic, message.c_str());
+  Serial.println("Message Published");
 }
